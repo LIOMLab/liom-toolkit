0c9e0b650676e22f6f83a8aced4b86bd1c5367af
Frans Irgolitsch <frans.irgolitsch@polymtl.ca>
2022-10-26 14:14:16 -0400

    Added comments

--- liom_toolkit/segmentation/plane_segmentation.py
+++ liom_toolkit/segmentation/plane_segmentation.py
@@ -9,6 +9,12 @@ from tqdm import tqdm
 
 
 def subtract_background(img, radius=70):
+    """
+    Subtract background from image using rolling ball algorithm
+    :param img: The image to subtract the background from
+    :param radius: The radius of the rolling ball
+    :return: The background subtracted image
+    """
     normalized_radius = radius // 255
     kernel = restoration.ellipsoid_kernel(
         (radius * 2, radius * 2),
@@ -18,30 +24,48 @@ def subtract_background(img, radius=70):
     return img - rolling_ball
 
 
-def remove_background(img, radius=70):
-    return subtract_background(img, radius=radius)
-
-
 def frangi_filter(img, sigma_range, black_ridges):
+    """
+    Apply the Frangi filter to an image
+    :param img: The image to apply the filter to
+    :param sigma_range: The range of sigmas to use
+    :param black_ridges: Whether to detect black ridges
+    :return: The filtered image
+    """
     return frangi(img, sigmas=[x for x in range(sigma_range)], black_ridges=black_ridges)
 
 
 def li_threshold_image(img):
+    """
+    Apply the Li thresholding algorithm to an image
+    :param img: The image to apply the thresholding to
+    :return: The thresholded image
+    """
     return img > thresholding.threshold_li(img, initial_guess=np.quantile(img, 0.95))
 
 
-def local_threshold_image(img):
-    return img > thresholding.threshold_local(img, block_size=501)
-
-
 def create_vessel_mask(img, background_filter_size=70, frangi_sigma_range=(2, 16, 2), frangi_black_ridges=False):
-    bg_less = remove_background(img, background_filter_size)
+    """
+    Create a vessel mask from an image
+    :param img: The image to create the mask from
+    :param background_filter_size: The size of the background filter
+    :param frangi_sigma_range: The range of sigmas to use for the Frangi filter
+    :param frangi_black_ridges: Whether to detect black ridges
+    :return: The vessel mask
+    """
+    bg_less = subtract_background(img, background_filter_size)
     frangi_image = frangi_filter(bg_less, frangi_sigma_range, frangi_black_ridges)
     thresholded_image = li_threshold_image(frangi_image)
     return thresholded_image, frangi_image
 
 
 def estimate_tissue_mask(img):
+    """
+    Estimate the tissue mask from an image
+    :param img: The image to estimate the mask from
+    :return: The tissue mask
+    Based on function found here: https://github.com/joe-from-mtl/sbhassisant-2d-3d-registration
+    """
     mask_data = img > 0
     # Get a tissue threshold value
     threshold_tissue = thresholding.threshold_triangle(img[mask_data])
@@ -74,11 +98,27 @@ def estimate_tissue_mask(img):
 
 
 def erode_mask(mask, disk_size=30):
+    """
+    Erode the outer edge of a mask
+    :param mask: The mask to erode
+    :param disk_size: The size of the disk to use for erosion
+    :return: The eroded mask
+    """
     return binary_erosion(mask, disk(disk_size))
 
 
 def segment_2d_images(base_directory, images, erode_mask_size=30, background_filter_size=70,
                       frangi_sigma_range=(2, 16, 2), frangi_black_ridges=False):
+    """
+    Segment 2D images
+    :param base_directory: The base directory to save the results to and load the images from
+    :param images: The filenames of the images to segment
+    :param erode_mask_size: The size of the disk to use for erosion
+    :param background_filter_size: The size of the background filter
+    :param frangi_sigma_range: The range of sigmas to use for the Frangi filter
+    :param frangi_black_ridges: Whether to detect black ridges
+    :return: The segmented images, both the binary vessel mask and the result of the Frangi filter
+    """
     pbar = tqdm(images)
     for image in pbar:
         # Read image
@@ -105,3 +145,4 @@ def segment_2d_images(base_directory, images, erode_mask_size=30, background_fil
         # Save image
         imsave(base_directory + image + '_vessel_mask.tif', vessel_mask)
         imsave(base_directory + image + '_frangi.tif', frangi)
+        return vessel_mask, frangi
