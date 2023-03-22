d86b19bde4ed5d55bbe282a3666f9a7c2d6393cc
Frans Irgolitsch <frans.irgolitsch@polymtl.ca>
2023-03-22 11:56:48 -0400

    Made memmap optional for loading hdf5 files

--- liom_toolkit/utils/conversion.py
+++ liom_toolkit/utils/conversion.py
@@ -187,10 +187,11 @@ def remove_stripe_based_wavelet_fft(image, level=5, sigma=1, order=8, pad=150):
     return image[:nrow, :ncol]
 
 
-def load_hdf5(hdf5_file, map_file="temp.dat"):
+def load_hdf5(hdf5_file, use_mem_map=True, map_file="temp.dat"):
     """
     Load a HDF5 into a numpy memmap.
     :param hdf5_file: The HDF5 file to load.
+    :param use_mem_map: Whether to use a memmap or not.
     :param map_file: The memmap file to save to.
     :return: The memmap.
     """
@@ -199,7 +200,10 @@ def load_hdf5(hdf5_file, map_file="temp.dat"):
         n_frames = len(keys)
         key_list = [base_key + "{:03d}".format(i + 1) for i in range(len(keys))]
         frame = file[key_list[0]][:]
-        data = np.memmap(map_file, dtype=np.uint16, mode='w+', shape=(n_frames, frame.shape[0], frame.shape[1]))
+        if use_mem_map:
+            data = np.memmap(map_file, dtype=np.uint32, mode='w+', shape=(n_frames, frame.shape[0], frame.shape[1]))
+        else:
+            data = np.zeros((n_frames, frame.shape[0], frame.shape[1]), dtype=np.uint32)
         for i, key in enumerate(
                 tqdm.tqdm(key_list, desc="Loading HDF5 file..", unit="frames", total=len(key_list),
                           leave=False, position=1)):
@@ -210,15 +214,16 @@ def load_hdf5(hdf5_file, map_file="temp.dat"):
     return data
 
 
-def convert_hdf5_to_nifti(hdf5_file, nifti_file):
+def convert_hdf5_to_nifti(hdf5_file, nifti_file, use_mem_map=True):
     """
     Convert a HDF5 file from the lightsheet microscope to a NIFTI file.
     :param hdf5_file: Path to the HDF5 file.
     :param nifti_file: Path to the NIFTI file.
+    :param use_mem_map: Whether to use a memmap or not.
     :return:
     """
     map_file = "temp.dat"
-    data = load_hdf5(hdf5_file, map_file)
+    data = load_hdf5(hdf5_file, use_mem_map, map_file)
 
     print("Saving...")
     ni_img = nib.Nifti1Image(data, affine=np.eye(4), dtype=np.uint16)
@@ -252,11 +257,13 @@ def save_zarr(data, zarr_file, remove_stripes=False, scales=(6.5, 6.5, 6.5), chu
     print("Done!")
 
 
-def convert_hdf5_to_zarr(hdf5_file, zarr_file, remove_stripes=False, scales=(6.5, 6.5, 6.5), chunks=(512, 512, 512)):
+def convert_hdf5_to_zarr(hdf5_file, zarr_file, use_mem_map=True, remove_stripes=False, scales=(6.5, 6.5, 6.5),
+                         chunks=(512, 512, 512)):
     """
     Convert a HDF5 file from the lightsheet microscope to a zarr file.
     :param hdf5_file: Path to the HDF5 file.
     :param zarr_file: Path to the zarr file.
+    :param use_mem_map: Whether to use a memmap or not.
     :param remove_stripes: Whether to remove stripes from the data.
     :param scales: The resolution of the image, in x y z order.
     :param chunks: The chunk size to use.
@@ -264,7 +271,7 @@ def convert_hdf5_to_zarr(hdf5_file, zarr_file, remove_stripes=False, scales=(6.5
     """
 
     map_file = "temp.dat"
-    data = load_hdf5(hdf5_file, map_file)
+    data = load_hdf5(hdf5_file, use_mem_map, map_file)
 
     save_zarr(data, zarr_file, remove_stripes, scales=scales, chunks=chunks)
     os.remove(map_file)
