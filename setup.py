9d8002c6378c7618e71827e9cfb483e56edb065d
Frans Irgolitsch <frans.irgolitsch@polymtl.ca>
2024-04-15 15:03:46 -0400

    Requirements update for yaml loading issue

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setup(
     python_requires='>=3.10',
     install_requires=['antspyx', 'tqdm', 'scikit-image', 'ome-zarr==0.8.2', 'nibabel', 'zarr', 'h5py', 'pynrrd',
                       'PyWavelets', 'SimpleITK', 'allensdk', 'dask', 'opencv-python', 'torch', 'torchvision', 'wandb',
-                      'patchify', 'natsort', 'albumentations']
+                      'patchify', 'natsort', 'albumentations', "ruamel.yaml==0.16.10"]
 )
