63d980526c3e8e8e0c34abb976eefbae31c0ceae
Frans Irgolitsch <frans.irgolitsch@polymtl.ca>
2024-05-29 15:24:34 -0400

    Explicitly set amount of dask workers in the local cluster

--- liom_toolkit/utils/dask_client.py
+++ liom_toolkit/utils/dask_client.py
@@ -1,6 +1,9 @@
+import multiprocessing
+
 from dask.distributed import LocalCluster
 
 # Setup default client for Dask. This is a global variable that can be used in any module.
-# This client is a local cluster that will use all threads available.
-cluster = LocalCluster()
+# This client is a local cluster that will use all threads available minus 1.
+cores = multiprocessing.cpu_count() - 1
+cluster = LocalCluster(n_workers=cores, threads_per_worker=1)
 client = cluster.get_client()
