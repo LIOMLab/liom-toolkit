e141fe06de3f22ac4bc3b9f5cea7096fded11779
Frans Irgolitsch <frans.irgolitsch@polymtl.ca>
2024-05-31 12:16:08 -0400

    Changed setup of client singleton to accept the creation of a connection to a remote dask cluster

--- liom_toolkit/utils/dask_client.py
+++ liom_toolkit/utils/dask_client.py
@@ -1,9 +1,36 @@
 import multiprocessing
 
-from dask.distributed import LocalCluster
+from dask.distributed import LocalCluster, Client
 
-# Setup default client for Dask. This is a global variable that can be used in any module.
-# This client is a local cluster that will use all threads available minus 1.
-cores = multiprocessing.cpu_count() - 1
-cluster = LocalCluster(n_workers=cores, threads_per_worker=1)
-client = cluster.get_client()
+global client
+
+
+def get_client(address=""):
+    if client is None and address == "":
+        create_local_cluster()
+    elif client is None and address != "":
+        connect_to_cluster(address)
+    return client
+
+
+def create_local_cluster():
+    """
+    Create a local cluster with the number of cores - 1
+
+    :return:
+    """
+    cores = multiprocessing.cpu_count() - 1
+    cluster = LocalCluster(n_workers=cores, threads_per_worker=1)
+    global client
+    client = cluster.get_client()
+
+
+def connect_to_cluster(address):
+    """
+    Connect to a cluster
+
+    :param address: The address of the cluster
+    :return: The client
+    """
+    global client
+    client = Client(address)
