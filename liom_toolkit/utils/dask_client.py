13769f8cc498fc1a1f1ec29cd31a177cd4f4850a
Frans Irgolitsch <frans.irgolitsch@polymtl.ca>
2024-05-31 13:46:58 -0400

    Wrapped client in a manager class

--- liom_toolkit/utils/dask_client.py
+++ liom_toolkit/utils/dask_client.py
@@ -2,35 +2,33 @@ import multiprocessing
 
 from dask.distributed import LocalCluster, Client
 
-global client
 
+class DaskClientManager:
+    def __init__(self):
+        self.client = None
 
-def get_client(address=""):
-    if client is None and address == "":
-        create_local_cluster()
-    elif client is None and address != "":
-        connect_to_cluster(address)
-    return client
+    def get_client(self, address=""):
+        if self.client is None and address == "":
+            self.client = self.create_local_cluster()
+        elif self.client is None and address != "":
+            self.client = self.connect_to_cluster(address)
+        return self.client
 
+    def create_local_cluster(self):
+        """
+        Create a local cluster with the number of cores - 1
 
-def create_local_cluster():
-    """
-    Create a local cluster with the number of cores - 1
+        :return:
+        """
+        cores = multiprocessing.cpu_count() - 1
+        cluster = LocalCluster(n_workers=cores, threads_per_worker=1)
+        return cluster.get_client()
 
-    :return:
-    """
-    cores = multiprocessing.cpu_count() - 1
-    cluster = LocalCluster(n_workers=cores, threads_per_worker=1)
-    global client
-    client = cluster.get_client()
+    def connect_to_cluster(self, address):
+        """
+        Connect to a cluster
 
-
-def connect_to_cluster(address):
-    """
-    Connect to a cluster
-
-    :param address: The address of the cluster
-    :return: The client
-    """
-    global client
-    client = Client(address)
+        :param address: The address of the cluster
+        :return: The client
+        """
+        return Client(address)
