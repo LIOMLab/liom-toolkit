5b13d5356cd0fc904d10ac3c2cfc0e3e8141e90d
Frans Irgolitsch <frans.irgolitsch@polymtl.ca>
2024-11-20 12:49:28 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="liom-toolkit",
-    version="0.8.15",
+    version="0.8.16",
     author="Laboratoire d’Imagerie Optique et Moléculaire",
     author_email="frederic.lesage@polymtl.ca",
     packages=find_packages(),
